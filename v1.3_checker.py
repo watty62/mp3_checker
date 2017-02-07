0c5fb48c050c5f4fb4abef2b82d3f4449e509993
Ian Watt <ianwatt@gmail.com>
2017-02-07 22:14:37 +0000

    improved checking and TSV output

--- v1.3_checker.py
+++ v1.3_checker.py
@@ -9,11 +9,11 @@ if test:
 else: 
   top = "/Volumes/music/MP3s/jazz"
 
-FileTypes=['mp3', 'm4a']
+FileTypes=['mp3', 'm4a', 'mp4', 'flac']
 
 cover_found = False
 
-f = open('mp3_log.txt', 'w')
+f = open('mp3_log.tsv', 'w')
 
 track_list = dict()
 
@@ -36,13 +36,17 @@ for root, dirs, files in os.walk(top, topdown=False):
     genre = root_path [5]
     artist = root_path [6]
     album = root_path [7]
-    f.write ("=================================== \n\n")
-    f.write ("Genre: " + genre + " \n")
-    f.write ("Artist: " + artist + " \n")
-    f.write ("No of track: " +  str(len (files)) + " \n")
-
+    #f.write ("=================================== \n\n")
+    #f.write ("Genre: " + genre + " \n")
+    #f.write ("Artist: '" + artist + "' \n")
+    #f.write ("No of track: " +  str(len (files)) + " \n")
+    f.write ("Artist \t Album \t Trackno \t Title \t Artist_match \t Album_match \t track_match \n"  )
     for fl in files:
       #print fl
+      album_match = True
+      artist_match = True
+      track_match = True
+      
       if fl.lower() == "folder.jpg":
         cover_found = True
       else:
@@ -55,15 +59,19 @@ for root, dirs, files in os.walk(top, topdown=False):
             track_name = re.findall("\S([a-z A-Z ' \( \)]*)\.\m", fl)[0]
             #print " Track name: ", track_name
             (id_artist, id_album, id_title, id_length, id_date, id_tracknum) = check_tags (currentFile)     
-
-            f.write ("Track " + track_no +": " + track_name +"\n")
+            
+            #f.write ("Track " + track_no +": " + track_name +"\n")
 
             if id_artist.strip().lower() != artist.strip().lower :
-              f.write("Filepath artist doesn't match ID3 Artist: " + id_artist+ "\n\n")
+              artist_match = False
+              #f.write("Filepath artist doesn't match ID3 Artist: '" + id_artist+ "'\n\n")
             if id_album.strip().lower() != album.strip().lower() :
-              f.write("Filepath album doesn't match ID3 album: " + id_album+ "\n\n")
-            if id_title.strip().lower() != track_name.strip().lower() :
-              f.write("Filepath track name doesn't match ID3 title: " + id_title+ "\n\n")
-
+              album_match = False
+              #f.write("Filepath album doesn't match ID3 album: " + id_album+ "\n\n")
+            if id_title.strip().lower() != track_name.strip().lower():
+              track_match = False 
+              #f.write("Filepath track name doesn't match ID3 title: " + id_title+ "\n\n")
+            
+            f.write(artist + "\t" + album + "\t" + id_tracknum + "\t" + track_name + "\t" + str(artist_match) + "\t" + str(album_match) + "\t" + str(track_match) + "\n" )
 f.close()
 
